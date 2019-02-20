2890c123b87ef2ca08d613b315b77d8407db6015
Matthew Shin <mshin1995@gmail.com>
2019-02-20 04:29:06 +0000

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,12 @@
 def count_elements(array)
-  # code goes here
+  new_hash = {}
+  array.each do |elements|
+    if new_hash[elements]
+      new_hash[elements] += 1
+    else
+      new_hash[elements] = 1
+    end
+  end
+  new_hash
 end
  
